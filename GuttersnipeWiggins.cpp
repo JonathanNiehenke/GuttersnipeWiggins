25e4b440e6b86ce83394a9c1f3e8a86197276b83
JonathanNiehenke <jrniehenke@gmail.com>
2016-08-26 10:38:12 -0700

    Empty ai module for testing ideas.

---
+++ GuttersnipeWiggins.cpp
@@ -0,0 +1,74 @@
+/* Sorry, initially a python programmer and C++ lacks a standard style
+enabling my accustomed line lengths of 72 for comments and 80 for code.
+*/
+#include "GuttersnipeWiggins.h"
+
+using namespace BWAPI::Filter;
+
+void GW::onStart()
+{
+}
+
+void GW::onFrame()
+{
+}
+
+void GW::onUnitCreate(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitMorph(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitComplete(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitDestroy(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitDiscover(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitEvade(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitShow(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitHide(BWAPI::Unit Unit)
+{
+}
+
+void GW::onUnitRenegade(BWAPI::Unit Unit)
+{
+}
+
+void GW::onNukeDetect(BWAPI::Position target)
+{
+}
+
+void GW::onSendText(std::string text)
+{
+}
+
+void GW::onReceiveText(BWAPI::Player player, std::string text)
+{
+}
+
+void GW::onPlayerLeft(BWAPI::Player player)
+{
+}
+
+void GW::onSaveGame(std::string gameName)
+{
+}
+
+void GW::onEnd(bool IsWinner)
+{
+}
